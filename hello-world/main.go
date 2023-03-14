71a082bfec46b75c53a8c631c198ab4e70c70ec5
Tyler <tylerclancy030@gmail.com>
2023-03-13 21:34:19 -0500

    Initial commit

---
+++ hello-world/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+// A few Go CLI commands
+/*
+go build - compiles Go source code files
+go run - compiles and executes one or two files
+go fmt - formats all the code in each file in current directory
+go install - compiles and installs a package
+go get - downloads the source code of someone else's package
+go test - runs any test files associated with current project
+*/
